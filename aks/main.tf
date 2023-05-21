cfdd0801a3fff61ca1b14154c4b976ea254611b6
oratar <121873526+oratar@users.noreply.github.com>
2023-05-21 09:48:36 +0300

    Update main.tf

--- aks/main.tf
+++ aks/main.tf
@@ -11,30 +11,23 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "learnk8sResourceGroup"
+  name     = "aksResourceGroup"
   location = "northeurope"
 }
 
 resource "azurerm_kubernetes_cluster" "cluster" {
-  name                = "learnk8scluster"
+  name                = "akscluster"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  dns_prefix          = "learnk8scluster"
+  dns_prefix          = "akscluster"
 
   default_node_pool {
     name       = "default"
     node_count = "2"
     vm_size    = "standard_d2_v2"
-    enable_node_public_ip = true
   }
 
   identity {
     type = "SystemAssigned"
-  }
-linux_profile {
-    admin_username = "ubuntu"
-    ssh_key {
-      key_data = file(var.ssh_public_key)
-    }
   }
 }

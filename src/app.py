a521dc455215057fa2418d699943aa60201a5f9f
oratar <121873526+oratar@users.noreply.github.com>
2023-05-23 17:27:25 +0300

    Update app.py

--- src/app.py
+++ src/app.py
@@ -25,7 +25,7 @@ app.config['SECRET_KEY'] = '661a50e0edbb39371492d2a3518cb98b94c7b22efc972448c8' 
 # MongoDB client
 mongo_usr = 'oratar333'  # MongoDB username
 mongo_pass = 'Aa123456' # MongoDB password
-client = pymongo.MongoClient(f"mongodb+srv://{monogo_usr}:{monogo_pass}@cluster0.xyggxii.mongodb.net/?retryWrites=true&w=majority")
+client = pymongo.MongoClient(f"mongodb+srv://{mongo_usr}:{mongo_pass}@cluster0.xyggxii.mongodb.net/?retryWrites=true&w=majority")
 collection = client.products.products
 
 # Product catalog
